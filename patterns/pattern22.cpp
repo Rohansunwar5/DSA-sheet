2a4c8af91d6db4f1084f9612b7b4bd454934636c
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-01-09 21:54:47 +0530

    pattern21,22

---
+++ patterns/pattern22.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printP(int n)
+{
+  for (int i = 0; i < 2 * n - 1; i++)
+  {
+    for (int j = 0; j < 2 * n - 1; j++)
+    {
+      int top = i;
+      int left = j;
+      int right = (2 * n - 2) - j;
+      int down = (2 * n - 2) - i;
+      cout << (n - min(min(top, down), min(left, right)));
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int n = 4;
+  printP(n);
+  return 0;
+}
--- patterns/tempCodeRunnerFile.cpp
+++ patterns/tempCodeRunnerFile.cpp
@@ -1,1 +1,8 @@
-cout << endl;
+#include <bits/stdc++.h>
+using namespace std;
+
+void printP(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
