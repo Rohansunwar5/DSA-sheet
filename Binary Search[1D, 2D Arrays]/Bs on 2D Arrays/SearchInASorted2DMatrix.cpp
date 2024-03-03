a1b926437ddbb227ce50b46f816a2271c1e079f5
Rohansunwar5 <80083391+Rohansunwar5@users.noreply.github.com>
2024-03-03 13:41:39 +0530

    bs on matrix

---
+++ Binary Search[1D, 2D Arrays]/Bs on 2D Arrays/SearchInASorted2DMatrix.cpp
@@ -0,0 +1,17 @@
+bool searchElement(vector<vector<int>> &MATRIX, int target)
+{
+  int n = MATRIX.size();
+  int m = MATRIX[0].size();
+
+  int row = 0, col = m - 1;
+  while (row < n && col >= 0)
+  {
+    if (MATRIX[row][col] == target)
+      return true;
+    else if (MATRIX[row][col] < target)
+      row++;
+    else
+      col--;
+  }
+  return false;
+}
